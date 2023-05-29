26e7b528bc27ddee6282ab89e88d2791e2a02e84
Thalita Marra <thalita.marra@grupoboticario.com.br>
2023-05-28 22:50:26 -0300

    Lint

--- src/routes/healthcheckRoutes.js
+++ src/routes/healthcheckRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import HealthcheckController from '../controllers/healthcheckController.js';
 
+// eslint-disable-next-line new-cap
 const healthcheckRouter = Router();
 
 healthcheckRouter.get('/healthcheck', HealthcheckController.healthcheck);
