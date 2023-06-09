435021a84db25836b1801e54b269397d6ca12485
Thalita Marra <thalita.marra@grupoboticario.com.br>
2023-06-09 00:23:11 -0300

    Setup da rota do swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const db = require('./config/database.js');
 const routes = require('./routes/index.js');
+const docs = require('../docs');
 
 db.on('error', console.error.bind(console, 'Database connection error'));
 db.once('open', () => {
@@ -10,6 +12,11 @@ db.once('open', () => {
 const app = express();
 
 app.use(express.json());
+app.use(
+    '/documentacao',
+    swaggerUi.serve,
+    swaggerUi.setup(docs, { explorer: true }),
+);
 
 routes(app);
 
